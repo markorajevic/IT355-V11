be3d9ae339950aa82920d9c1abedf4ab50ef26a5
Marko Rajevic <me@markorajevic.space>
2018-12-17 16:36:38 +0100

    first commit

---
+++ src/main/java/com/metropolitan/it355vezba11/config/JmsConfig.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.metropolitan.it355vezba11.config;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+
+/**
+ *
+ * @author Vladimir Milicevic
+ */
+@Configuration
+@EnableJms
+@ComponentScan(basePackages = "com.metropolitan.it355vezba11")
+public class JmsConfig {
+
+    String BROKER_URL = "tcp://localhost:61616";
+    String BROKER_USERNAME = "admin";
+    String BROKER_PASSWORD = "admin";
+
+    @Bean
+    public ActiveMQConnectionFactory connectionFactory() {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
+        connectionFactory.setBrokerURL(BROKER_URL);
+        connectionFactory.setPassword(BROKER_USERNAME);
+        connectionFactory.setUserName(BROKER_PASSWORD);
+        return connectionFactory;
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate() {
+        JmsTemplate template = new JmsTemplate();
+        template.setConnectionFactory(connectionFactory());
+        return template;
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory());
+        factory.setConcurrency("1-1");
+        return factory;
+    }
+}
---
+++ src/main/java/com/metropolitan/it355vezba11/jms/Listener.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.metropolitan.it355vezba11.jms;
+
+import org.springframework.stereotype.Component;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.TextMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+
+/**
+ *
+ * @author Vladimir Milicevic
+ */
+@Component
+public class Listener {
+
+    @Autowired
+    private Producer producer;
+
+    @JmsListener(destination = "inbound.queue")
+    public void receiveMessage(final Message jsonMessage) throws JMSException {
+        String messageData = null;
+        System.out.println("Received message " + jsonMessage);
+        if (jsonMessage instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage) jsonMessage;
+            messageData = textMessage.getText();
+        }
+        producer.sendMessage("outbound.queue", messageData);
+    }
+}
